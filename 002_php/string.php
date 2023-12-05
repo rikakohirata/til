a944e806533d4bf7a2f270b568a0aa142550e642
rikakohirata <rikako.hirata88@gmail.com>
2023-12-05 19:09:42 +0900

    add: strtr — 文字の変換あるいは部分文字列の置換を行う

--- 002_php/string.php
+++ 002_php/string.php
@@ -13,3 +13,31 @@ var_dump($number === $reverse_number); // => bool(true)
 $string = "ABCDEF";
 print_r(str_split($string));    // => [A, B, C, D, E, F]
 print_r(str_split($string, 2)); // => [AB, CD, EF]
+
+/**
+ * strtr — 文字の変換あるいは部分文字列の置換を行う
+ *  引数を2つ渡した場合、第一引数の文字列と、第二引数の配列のキーを比較し、一致したキーの値に置換する
+ *  第二引数はarray(key => value)形式で渡す
+ */
+$s = "MCMXCIV";
+$s = strtr(
+  $s,
+  [
+    'M'  => '1000,',
+    'CM' => '900,',
+    'D'  => '500,',
+    'CD' => '400,',
+    'C'  => '100,',
+    'XC' => '90,',
+    'L'  => '50,',
+    'XL' => '40,',
+    'X'  => '10,',
+    'IX' => '9,',
+    'V'  => '5,',
+    'IV' => '4',
+    'I'  => '1,'
+  ]
+);
+var_dump($s);
+$sum = array_sum(explode(',', $s));
+var_dump($sum); // => int(1994)
