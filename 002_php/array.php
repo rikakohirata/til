e42b3da07b82d5b30bba1f951798c058e529d7c9
rikakohirata <rikako.hirata88@gmail.com>
2023-12-05 21:03:07 +0900

    add: implode — 配列要素を文字列により連結する

--- 002_php/array.php
+++ 002_php/array.php
@@ -5,3 +5,12 @@
  */
 $samples = [0, 1, 2];
 print_r(array_reverse($samples)); // => [2, 1, 0]
+
+/**
+ * implode — 配列要素を文字列により連結する
+ *  第一引数：連結したい文字列の配列
+ *  第二引数：指定した文字列で配列の要素を連結する、デフォルトは空文字列
+ */
+$words = ['n', 'a', 'm', 'e'];
+var_dump(implode($words));      // => string(4) "name"
+var_dump(implode(",", $words)); // => string(7) "n,a,m,e"
