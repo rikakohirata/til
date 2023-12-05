af6d34de48ebd0dce45de64a30197de47aa5f55e
rikakohirata <rikako.hirata88@gmail.com>
2023-12-05 18:11:00 +0900

    add: str_split — 文字列を配列に変換する

--- 002_php/string.php
+++ 002_php/string.php
@@ -6,3 +6,10 @@
 $number = 12321;
 $reverse_number = (int)strrev($number);
 var_dump($number === $reverse_number); // => bool(true)
+
+/**
+ * str_split — 文字列を配列に変換する
+ */
+$string = "ABCDEF";
+print_r(str_split($string));    // => [A, B, C, D, E, F]
+print_r(str_split($string, 2)); // => [AB, CD, EF]
